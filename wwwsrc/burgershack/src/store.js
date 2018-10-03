a1f562291af3346706aecca1de2032637a6566e7
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-10-03 11:08:02 -0600

    following along

--- wwwsrc/burgershack/src/store.js
+++ wwwsrc/burgershack/src/store.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import Axios from 'axios';
+
+var server = Axios({
+  baseURL: "//localhost:5000/api"
+})
+
 
 Vue.use(Vuex)
 
